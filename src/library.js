552f23a2a14210082597edb27ea67f183471a7b2
Shaun <skichenbrand@gmail.com>
2026-04-17 14:32:08 +0200

    work

--- src/library.js
+++ src/library.js
@@ -6,6 +6,9 @@ import propsRepeaterGrid from './components/repeaterGrid/props';
 
 import { registerComponentTypes } from '@intenda/opus-ui';
 
+//Components
+export * from './libraryComponents';
+
 registerComponentTypes([{
 	type: 'repeaterGrid',
 	component: RepeaterGrid,
